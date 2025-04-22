6d3f9801eb921d4f6b17246279be1376683f044e
Jinghao Jia <jinghao7@illinois.edu>
2025-04-22 01:46:45 -0500

    xdp-handler: format using kernel style
    Signed-off-by: Jinghao Jia <jinghao7@illinois.edu>

--- xdp-handler/fast_common.h
+++ xdp-handler/fast_common.h
@@ -14,8 +14,8 @@
 
 #define ETH_ALEN 6 /* Octets in one ethernet addr	 */
 
-#define CLUSTER_SIZE 3        // need
-#define FAST_REPLICA_MAX 100  // max # of replicas.
+#define CLUSTER_SIZE 3 // need
+#define FAST_REPLICA_MAX 100 // max # of replicas.
 #define NONFRAG_MAGIC 0x20050318
 #define FRAG_MAGIC 0x20101010
 
@@ -28,28 +28,25 @@
 #define FAST_PAXOS_DATA_LEN 12
 #define BROADCAST_SIGN_BIT (1 << 31)
 #define QUORUM_SIZE ((CLUSTER_SIZE + 1) >> 1)
-#define QUORUM_BITSET_ENTRY 1024  // must be 2^t
+#define QUORUM_BITSET_ENTRY 1024 // must be 2^t
 
 enum ReplicaStatus { STATUS_NORMAL, STATUS_VIEW_CHANGE, STATUS_RECOVERING };
 
 enum {
-  FAST_PROG_XDP_HANDLE_PREPARE = 0,
-  FAST_PROG_XDP_HANDLE_REQUEST,
-  FAST_PROG_XDP_HANDLE_PREPAREOK,
-  FAST_PROG_XDP_WRITE_BUFFER,
-  FAST_PROG_XDP_PREPARE_REPLY,
-  FAST_PROG_XDP_MAX
+	FAST_PROG_XDP_HANDLE_PREPARE = 0,
+	FAST_PROG_XDP_HANDLE_REQUEST,
+	FAST_PROG_XDP_HANDLE_PREPAREOK,
+	FAST_PROG_XDP_WRITE_BUFFER,
+	FAST_PROG_XDP_PREPARE_REPLY,
+	FAST_PROG_XDP_MAX
 };
 
-enum {
-  FAST_PROG_TC_BROADCAST = 0,
-  FAST_PROG_TC_MAX
-};
+enum { FAST_PROG_TC_BROADCAST = 0, FAST_PROG_TC_MAX };
 
 struct paxos_configure {
-  __u32 addr;  // ipv4.
-  __u16 port;
-  char eth[ETH_ALEN];
+	__u32 addr; // ipv4.
+	__u16 port;
+	char eth[ETH_ALEN];
 };
 
 #endif
